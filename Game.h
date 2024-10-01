f369beed3bf81ba597c26ab76d9487513115eae6
a1924627 <a1924627@adelaide.edu.au>
2024-10-01 12:18:08 +0930

    Sharing files so it works

---
+++ Game.h
@@ -0,0 +1,23 @@
+#ifndef GAME_H
+#define GAME_H
+
+// Include libraries
+#include <iostream>
+#include <string>
+
+// Include classes
+#include "Location.h"
+
+
+using namespace std;
+
+class Game {
+    private:
+        int bossesDefeated;
+        Location currentLocation; 
+        bool playerAlive; 
+
+    public:
+};
+
+#endif
--- Location.cpp
+++ Location.cpp
@@ -2,5 +2,17 @@
 
 Location::Location()
 {
-    
+
+}
+
+void Location::travelToLocation(string destination)
+{
+}
+
+void Location::showLocation()
+{
+}
+
+Location::Location(string name, string description, string asciiDescription)
+{
 }
