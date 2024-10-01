2b48f44d42f8461931958ea356b7d3466d5ff99b
a1924627 <a1924627@adelaide.edu.au>
2024-10-01 12:45:32 +0930

    Added all functions from project plan

--- Location.h
+++ Location.h
@@ -5,16 +5,17 @@
 #include <string> 
 using namespace std; 
 
-class Location {
+class Location : public Game {
     private:
+        // Variables describing a location
         string name; 
         string description;
         string asciiDescription; 
     public:
-        Location(); 
-        Location(string name, string description, string asciiDescription); 
-        void showLocation(); 
-        void travelToLocation(string destination);
+        Location(); // Default constructor 
+        Location(string name, string description, string asciiDescription); // Constructor 
+        void showLocation(); // Displays the locations details 
+        void travelToLocation(string destination); // Changes the location 
 };
 
 #endif 
