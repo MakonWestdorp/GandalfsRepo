6a7174f35502f6b2df5d25faac8a7ba689eb7d4b
MakonWestdorp <makon.westdorp@student.adelaide.edu.au>
2024-10-01 18:40:06 +0930

    Changed the Game Class to make Entity a friend,giving Entity access to the Bosses Defeated variable

--- Game.h
+++ Game.h
@@ -11,28 +11,35 @@
 // Namespace
 using namespace std;
 
+// Forward Declaring Entity class to allow Game to declare Entity as a friend
+// class give access to bossesDefeated variable
+class Entity;
 class Game {
-    private:
-        // Variables for keeping track of the game
-        int bossesDefeated;
-        Location currentLocation; 
-        bool playerAlive; 
-    protected:
-        Game(); // Default constructor
-        Game(int bossesDefeated, Location location, bool playerAlive); // Constructor
-        bool initialiseShops(); // Initialises shops
-        bool initialiseLocations(); // Initialises locations
-        void playTutorial(); // Plays the tutorial 
-        void openCharacterCreator(); // Runs the character creator
-        bool saveGame(); // Saves the game
-        void exitGame(); // Exits the game
-        bool loadGame(); // Loads the game
-        void endGame(); // Ends the game if the player dies
-    public: 
-        bool travelToLocation(Location destination); // Switches location
-        bool travelToShop(); // Switches to a shop
-        void setCurrentLocation(Location currentLocation); // Sets current location
-        Location viewCurrentLocation(); // Returns current location
+  // declaring Entity as a Friend Class
+  friend class Entity;
+
+ private:
+  // Variables for keeping track of the game
+  int bossesDefeated;
+  Location currentLocation;
+  bool playerAlive;
+
+ protected:
+  Game();  // Default constructor
+  Game(int bossesDefeated, Location location, bool playerAlive);  // Constructor
+  bool initialiseShops();       // Initialises shops
+  bool initialiseLocations();   // Initialises locations
+  void playTutorial();          // Plays the tutorial
+  void openCharacterCreator();  // Runs the character creator
+  bool saveGame();              // Saves the game
+  void exitGame();              // Exits the game
+  bool loadGame();              // Loads the game
+  void endGame();               // Ends the game if the player dies
+ public:
+  bool travelToLocation(Location destination);        // Switches location
+  bool travelToShop();                                // Switches to a shop
+  void setCurrentLocation(Location currentLocation);  // Sets current location
+  Location viewCurrentLocation();  // Returns current location
 };
 
 #endif
