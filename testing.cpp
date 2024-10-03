519838bda8a66238dcedb12e36c76743083e40a7
a1924627 <a1924627@adelaide.edu.au>
2024-10-03 17:39:43 +0930

    Added interface for testing

--- testing.cpp
+++ testing.cpp
@@ -10,6 +10,34 @@
 
 int main() {
     UnitTest test; 
-    test.StartShopTests();
+    int testSelector = 0; 
+    string divider = "+------------------------------------------------------------------+";
+
+    cout << divider << endl << "Select which test to run" << endl << divider << endl << "1. All tests" 
+         << endl << "2. Entity Tests" << endl << "3. Location Tests" << endl << "4. Shop Tests" 
+         << endl << divider << endl;
+
+    cin >> testSelector; 
+    cout << "Test begin" << endl; 
+
+    switch (testSelector) {
+        case 1: 
+            test.StartAllTests();
+            break;
+        case 2:
+            test.StartEntityTests();
+            break;
+        case 3:
+            test.StartLocationTests();
+            break;
+        case 4:
+            test.StartShopTests();
+            break;
+        default: 
+            break;
+    }
+
+    cout << endl << "Test Complete" << endl << divider;
+
     return 0; 
 }
