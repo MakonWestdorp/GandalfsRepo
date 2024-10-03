fd2cb6f24de8b1ec10a20110c6745121922816c7
a1924627 <a1924627@adelaide.edu.au>
2024-10-03 20:06:48 +0930

    Created bare bones Forest class

---
+++ Forest.cpp
@@ -0,0 +1,5 @@
+#include "Forest.h"
+
+Forest::Forest() : Location()
+{
+}
---
+++ Forest.h
@@ -0,0 +1,14 @@
+#ifndef FOREST_H
+#define FOREST_H
+
+#include <iostream>
+#include <string>
+#include "Location.h"
+
+class Forest : public Location {
+    private:
+    public:
+        Forest();
+}; 
+
+#endif
