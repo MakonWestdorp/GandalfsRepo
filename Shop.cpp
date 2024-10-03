f6fc09e0a2cf289b9e75b74d78b716972a1d0eb0
a1924627 <a1924627@adelaide.edu.au>
2024-10-03 12:54:41 +0930

    Shop features almost complete

--- Shop.cpp
+++ Shop.cpp
@@ -1,1 +1,109 @@
 #include "Shop.h"
+
+Shop::Shop()
+{
+    this->inventory = nullptr; 
+    this->prices = nullptr; 
+    this->forSaleStatus = nullptr;
+    this->shopName = "No name";
+    this->shopKeeperName = "No Name"; 
+    this->description = "No description";
+    this->asciiDescription = "No ascii description"; 
+    this->shopLevel = 0; 
+    this->ownerAnnoyanceLevel = 0; 
+    this->inventorySize = 0;
+
+}
+
+Shop::Shop(BaseItem *inventory, int *prices, bool *forSaleStatus, string shopName, string shopKeeperName, string description, string asciiDescription)
+{
+    this->inventory = inventory; 
+    this->prices = prices; 
+    this->forSaleStatus = forSaleStatus;
+    this->shopName = shopName;
+    this->shopKeeperName = shopKeeperName; 
+    this->description = description;
+    this->asciiDescription = asciiDescription; 
+    this->shopLevel = 1; 
+    this->ownerAnnoyanceLevel = 0; 
+    this->inventorySize = 5; 
+}
+
+BaseItem *Shop::viewInventory()
+{
+    return this->inventory;
+}
+
+int *Shop::viewPrices()
+{
+    return this->prices;
+}
+
+bool *Shop::viewForSaleStatus()
+{
+    return this->forSaleStatus;
+}
+
+string Shop::purchaseItem(int itemNumber)
+{
+    int cashOnHand = 10; // Temporary variable as player class does not yet exist
+
+    // Checking if item exists
+    if (itemNumber > inventorySize) { 
+        this->ownerAnnoyanceLevel += 0.5; // Owner will be annoyed if user input is invalid
+        return "Item does not exist";
+
+    } else {
+        itemNumber -= 1; // Decreases item number by 1 so it can be used to access the correct element of the array
+
+        if (forSaleStatus[itemNumber] == false) {
+            return "Item not for sale"; 
+
+        } else {
+            if (prices[itemNumber] > cashOnHand) {
+                return "Insufficient funds";
+
+            } else {
+                forSaleStatus[itemNumber] = false;
+                cashOnHand -= prices[itemNumber];
+                return "Item purchased";
+            }
+        }
+
+        return "Error"; 
+    }
+
+    
+
+}
+
+void Shop::showShopInterface()
+{
+    // Divider to seperate elements of the visual
+    string divider = "+------------------------------------------------------------------+";
+
+    // Clears terminal
+    cout << endl << endl << endl << endl << endl << endl; 
+
+    // Show shop visuals 
+    cout << divider << endl << this->shopName << "  : " << this->shopKeeperName << endl 
+         << divider << endl << this->description << endl << divider << endl; 
+
+    // Show shop options
+    for (int i = 0; i < inventorySize; i++) {
+        cout << i+1 << ". " << endl ;
+    }
+}
+
+void Shop::updateShop(BaseItem *inventory, int *prices, bool *forSaleStatus)
+{
+    // Delete old inventory
+    delete this->inventory; 
+    delete this->prices; 
+    delete this->forSaleStatus; 
+
+    // Implement new inventory
+    this->inventory = inventory; 
+    this->prices = prices; 
+    this->forSaleStatus = forSaleStatus;
+}
--- Shop.h
+++ Shop.h
@@ -1,7 +1,37 @@
 #ifndef SHOP_H
 #define SHOP_H
 
+// Include libraries
+#include <iostream>
+#include <string> 
+
+using namespace std; 
+
+// Temporary forward declare as BaseItem class does not yet exist
+class BaseItem; 
+
 class Shop {
+    private:
+        // Inventory variables
+        BaseItem *inventory; 
+        int *prices;
+        bool *forSaleStatus;  
+        // Description variables
+        string shopName, shopKeeperName, description, asciiDescription; 
+        // Other variables
+        int shopLevel, inventorySize; 
+        float ownerAnnoyanceLevel; 
+    public:
+        Shop(); // Default Constructor 
+        Shop(BaseItem *inventory, int *prices, bool *forSaleStatus, string shopName, 
+             string shopKeeperName, string description, string asciiDescription); // Constructor 
+        BaseItem* viewInventory(); // Returns inventory
+        int* viewPrices(); // Returns prices
+        bool* viewForSaleStatus(); // Returns for sale statuses 
+        string purchaseItem(int itemNumber); // Allows user to purchase item
+        void showShopInterface(); // Shows the shop interface and items
+        void updateShop(BaseItem *inventory, int *prices, bool *forSaleStatus); // Updates inventory
+
 
 };
 #endif
