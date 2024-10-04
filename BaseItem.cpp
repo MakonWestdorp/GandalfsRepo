7934ac4176b3e1ba5239eb167d4c59855bc66c70
MakonWestdorp <makon.westdorp@student.adelaide.edu.au>
2024-10-04 11:05:12 +0930

    Item name randomiser

--- BaseItem.cpp
+++ BaseItem.cpp
@@ -1,16 +1,47 @@
 #include "BaseItem.h"
 
-BaseItem::BaseItem()
-{
-    this->name = "No name";
-}
+BaseItem::BaseItem() { name = nameRandomiser(); }
 
-BaseItem::BaseItem(string name)
-{
-    this->name = name; 
-}
+BaseItem::BaseItem(string inputName) { name = inputName; }
 
-string BaseItem::getName()
-{
-    return this->name;
+string BaseItem::getName() { return name; }
+string getRandomElement(const string elements[], int size) {
+  return elements[rand() % size];
+}
+string BaseItem::nameRandomiser() {
+  // 1% chance for a rare prefix
+  bool isRare = (rand() % 100) < 1;
+
+  // Array of rare prefixes
+  const std::string rarePrefixes[] = {"Ancient", "Great", "Legendary",
+                                      "Mythic"};
+  int rarePrefixesSize = sizeof(rarePrefixes) / sizeof(rarePrefixes[0]);
+
+  // Array of weapon types
+  const std::string weapons[] = {"Katana", "Nunchucks", "Sword", "Bow",
+                                 "Axe",    "Dagger",    "Staff"};
+  int weaponsSize = sizeof(weapons) / sizeof(weapons[0]);
+
+  // Array of adjectives
+  const std::string adjectives[] = {"Doom",       "Fury",    "the Phoenix",
+                                    "Vengeance",  "Thunder", "the Dragon",
+                                    "Destruction"};
+  int adjectivesSize = sizeof(adjectives) / sizeof(adjectives[0]);
+
+  // Generate the weapon name
+  std::string weapon = getRandomElement(weapons, weaponsSize);
+
+  // Generate the adjective
+  std::string adjective = getRandomElement(adjectives, adjectivesSize);
+
+  // Construct the final item name
+  std::string itemName;
+  if (isRare) {
+    itemName = getRandomElement(rarePrefixes, rarePrefixesSize) + " " + weapon +
+               " of " + adjective;
+  } else {
+    itemName = weapon + " of " + adjective;
+  }
+
+  return itemName;
 }
--- BaseItem.h
+++ BaseItem.h
@@ -7,12 +7,15 @@
 using namespace std;
 
 class BaseItem {
-    private:
-        string name; 
-    public:
-        BaseItem(); // Defeault Constructor
-        BaseItem(string name); // Constructor 
-        string getName(); // Returns name
+ private:
+  string name;
+
+ public:
+  BaseItem();               // Defeault Constructor
+  BaseItem(string name);    // Constructor
+  string getName();         // Returns name
+  string nameRandomiser();  // Generates a random name for the item.
+  string getRandomElement(const string elements[], int size);
 };
 
 #endif
