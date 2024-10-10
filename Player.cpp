f290ee0ebeae81a66bd411421f7cb658c75f453c
MakonWestdorp <makon.westdorp@student.adelaide.edu.au>
2024-10-10 11:47:37 +1030

    Player changes

--- Player.cpp
+++ Player.cpp
@@ -1,6 +1,111 @@
 #include "Player.h"
 
-Player::Player() {}
+#include <limits>
+Player::Player() {
+  int NumberOfPointsToDistrubute = 20;
+  strength = 0;
+  healthPoints = 0;
+  defence = 0;
+  magic = 0;
+  string divider =
+      "+------------------------------------------------------------------+";
+  cout << endl << endl << endl << endl << endl << endl << endl << endl;
+  cout << divider << endl
+       << "Welcome to the Character Creator!" << endl
+       << "You have:" << NumberOfPointsToDistrubute
+       << " to distribute among your Character Stats." << endl
+       << divider << endl;
+  cout << "Yours Character has 4 Stats.Health Points, Strength, Defence and "
+          "Magic."
+       << endl;
+
+  while (true) {
+    cout << "You have: " << NumberOfPointsToDistrubute
+         << "left to distribute to your stats." << endl;
+    cout << "How many points would you like to allocate to your Health Points?"
+         << endl;
+    // READ IN USER INPUT FOR HP;
+    int UserInput;
+
+    while (true) {
+      std::cin >> UserInput;
+
+      if (std::cin.fail() || UserInput < 0 ||
+          UserInput > NumberOfPointsToDistrubute) {
+        std::cin.clear();  // clear the error flag
+        std::cout
+            << "That doesnt look right! Please enter a number between 0 and "
+            << NumberOfPointsToDistrubute << ": ";
+      } else {
+        break;  // valid input, exit loop
+      }
+    }
+    healthPoints += UserInput;
+    NumberOfPointsToDistrubute = NumberOfPointsToDistrubute - UserInput;
+    cout << "You have: " << NumberOfPointsToDistrubute
+         << "left to distribute to your stats." << endl;
+    cout << "How many points would you like to allocate to Strength?" << endl;
+    // READ IN USER INPUT FOR STR;
+    while (true) {
+      std::cin >> UserInput;
+
+      if (std::cin.fail() || UserInput < 0 ||
+          UserInput > NumberOfPointsToDistrubute) {
+        std::cin.clear();  // clear the error flag
+        std::cout
+            << "That doesnt look right! Please enter a number between 0 and "
+            << NumberOfPointsToDistrubute << ": ";
+      } else {
+        break;  // valid input, exit loop
+      }
+    }
+    strength += UserInput;
+    NumberOfPointsToDistrubute = NumberOfPointsToDistrubute - UserInput;
+    cout << "You have: " << NumberOfPointsToDistrubute
+         << "left to distribute to your stats." << endl;
+    cout << "How many points would you like to allocate to Defence?" << endl;
+    // READ IN USER INPUT FOR DEF;
+    while (true) {
+      std::cin >> UserInput;
+
+      if (std::cin.fail() || UserInput < 0 ||
+          UserInput > NumberOfPointsToDistrubute) {
+        std::cin.clear();  // clear the error flag
+        std::cout
+            << "That doesnt look right! Please enter a number between 0 and "
+            << NumberOfPointsToDistrubute << ": ";
+      } else {
+        break;  // valid input, exit loop
+      }
+    }
+    defence += UserInput;
+    NumberOfPointsToDistrubute = NumberOfPointsToDistrubute - UserInput;
+    cout << "You have: " << NumberOfPointsToDistrubute
+         << "left to distribute to your stats." << endl;
+    cout << "How many points would you like to allocate to Magic?" << endl;
+    // READ IN USER INPUT FOR MAG;
+    while (true) {
+      std::cin >> UserInput;
+
+      if (std::cin.fail() || UserInput < 0 ||
+          UserInput > NumberOfPointsToDistrubute) {
+        std::cin.clear();  // clear the error flag
+        std::cout
+            << "That doesnt look right! Please enter a number between 0 and "
+            << NumberOfPointsToDistrubute << ": ";
+      } else {
+        break;  // valid input, exit loop
+      }
+    }
+    magic += UserInput;
+    NumberOfPointsToDistrubute = NumberOfPointsToDistrubute - UserInput;
+    cout << "You have: " << NumberOfPointsToDistrubute
+         << "left to distribute to your stats." << endl;
+    if (NumberOfPointsToDistrubute == 0) {
+      break;
+    }
+  }
+}
 
 Player::Player(string inputName, int inputHP, int inputSTR, int inputDEF,
                int inputMAG, string inputResistance) {
@@ -16,7 +121,7 @@ Player::Player(string inputName, int inputHP, int inputSTR, int inputDEF,
 void Player::TakeTurn() {
   string divider =
       "+------------------------------------------------------------------+";
-  cout << endl << endl << endl << endl << endl << endl;
+  cout << endl << endl << endl << endl << endl << endl << endl << endl;
   cout << divider << endl
        << "   |   Character Name : " << this->name
        << "   |   Current HP: " << healthPoints << endl
