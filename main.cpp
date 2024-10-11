9bfaac980ece76e8c9b35052df5bf112b46f96d3
MakonWestdorp <makon.westdorp@student.adelaide.edu.au>
2024-10-11 17:44:00 +1030

    Thank you message at the end of the main file

--- main.cpp
+++ main.cpp
@@ -3,38 +3,43 @@
 #include <string>
 
 // Include classes
+#include "Arena.h"
+#include "AttackItem.h"
+#include "BaseItem.h"
+#include "BuffItem.h"
+#include "Enemy.h"
+#include "Entity.h"
+#include "Forest.h"
 #include "Game.h"
 #include "Location.h"
-#include "BaseItem.h"
-#include "Entity.h"
-#include "Shop.h"
-#include "AttackItem.h"
-#include "BuffItem.h"
-#include "Town.h"
-#include "Forest.h"
-#include "Arena.h"
 #include "Player.h"
-#include "Enemy.h"
+#include "Shop.h"
+#include "Town.h"
 
 int main() {
-    // Create game
-    Game ChampionsOfSand;
-    int numBossesDefeated = 0;
+  // Create game
+  Game ChampionsOfSand;
+  int numBossesDefeated = 0;
 
-    // Initialies shops and locations
-    ChampionsOfSand.initialiseShops();
-    ChampionsOfSand.initialiseLocations();
+  // Initialies shops and locations
+  ChampionsOfSand.initialiseShops();
+  ChampionsOfSand.initialiseLocations();
 
-    // Start Game menu
-    
-    // New || Load Game
-    Player *player;
-    // Tutorial 
+  // Start Game menu
 
-    // Game Loop
-    ChampionsOfSand.setCurrentLocation(ChampionsOfSand.viewLocations()[0]);
+  // New || Load Game
+  Player *player;
+  // Tutorial
 
-    while (ChampionsOfSand.isGameRunning() == true) {
-    ChampionsOfSand.viewCurrentLocation()->showLocation(ChampionsOfSand, player, numBossesDefeated);
-    }
+  // Game Loop
+  ChampionsOfSand.setCurrentLocation(ChampionsOfSand.viewLocations()[0]);
+
+  while (ChampionsOfSand.isGameRunning() == true) {
+    ChampionsOfSand.viewCurrentLocation()->showLocation(ChampionsOfSand, player,
+                                                        numBossesDefeated);
+  }
+  if (!player->GetIsAlive()) {
+    cout << "Game Over! We hope you enjoyed this game of Champions of the Sand!"
+         << endl;
+  }
 }
