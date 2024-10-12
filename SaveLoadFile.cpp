81c7a0a2c4e172d9a3a38afda3725a76cc0b7c69
a1924627 <a1924627@adelaide.edu.au>
2024-10-12 20:37:27 +1030

    Creating and reading in file both working

--- SaveLoadFile.cpp
+++ SaveLoadFile.cpp
@@ -9,19 +9,26 @@ int main() {
     string name = "Timmy", resistance = "Magic";
     int health = 5, strength = 3, defence = 8, magic = 4, bossesDefeated = 2;
 
-    ofstream outputFile("player.txt");
+    ofstream playerFileOut("player.txt");
 
-    if (outputFile.is_open()) {
+    if (playerFileOut.is_open()) {
         // Write values to file on different lines
-        outputFile << name << endl;
-        outputFile << health << endl;
-        outputFile << strength << endl;
-        outputFile << defence << endl;
-        outputFile << magic << endl;
-        outputFile << resistance << endl;
-        outputFile << bossesDefeated << endl;
+        playerFileOut << "Name:\n";
+        playerFileOut << name << endl;
+        playerFileOut << "Health:\n";
+        playerFileOut << health << endl;
+        playerFileOut << "Strength:\n";
+        playerFileOut << strength << endl;
+        playerFileOut << "Defence:\n";
+        playerFileOut << defence << endl;
+        playerFileOut << "Magic:\n";
+        playerFileOut << magic << endl;
+        playerFileOut << "Resistance:\n";
+        playerFileOut << resistance << endl;
+        playerFileOut << "Bosses_defeated:\n";
+        playerFileOut << bossesDefeated << endl;
 
-        outputFile.close();
+        playerFileOut.close();
 
         cout << "Values written to file" << endl;
     } else 
@@ -32,6 +39,38 @@ int main() {
     // Reading values
     name = "No Name", resistance = "No Resistance";
     health = 0, strength = 0, defence = 0, magic = 0, bossesDefeated = 0;
+
+     cout << name << endl << health << endl << strength << endl 
+         << defence << endl << magic << endl << resistance << endl 
+        << bossesDefeated << endl << endl;
+
+    ifstream playerFileIn("player.txt");
+
+    if (playerFileIn.is_open()) {
+        string commentsHolder = "";
+        playerFileIn >> commentsHolder;
+        playerFileIn >> name;
+        playerFileIn >> commentsHolder;
+        playerFileIn >> health;
+        playerFileIn >> commentsHolder;
+        playerFileIn >> strength;
+        playerFileIn >> commentsHolder;
+        playerFileIn >> defence;
+        playerFileIn >> commentsHolder;
+        playerFileIn >> magic;
+        playerFileIn >> commentsHolder;
+        playerFileIn >> resistance;
+        playerFileIn >> commentsHolder;
+        playerFileIn >> bossesDefeated;
+
+        playerFileIn.close();
+    } else {
+        cout << "File could not be opened" << endl;
+    }
+
+    cout << name << endl << health << endl << strength << endl 
+         << defence << endl << magic << endl << resistance << endl 
+        << bossesDefeated << endl;
 
     return 0;
 }
